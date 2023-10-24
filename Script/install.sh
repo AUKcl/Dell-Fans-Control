051b8d48b517173d750647f782201c26211a67b0
AUKcl <kaixuan135@outlook.com>
2023-10-24 23:30:01 +0800

    完善日志函数

--- Script/install.sh
+++ Script/install.sh
@@ -37,6 +37,7 @@ log_message() {
 # 函数：捕获异常并记录到日志
 try_catch() {
     if `"$@"`; then
+        log_message "Command '$*' executed successfully."
         return 0
     else
         local exit_code=$?
